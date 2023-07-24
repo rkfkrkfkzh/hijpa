d599fc7916dfb13cd652c53ae8c3f78fae839db7
imhyojin <1004dlagywls@naver.com>
2023-07-24 17:38:10 +0900

    묵시적 조인말고 명시적으로 사용하자

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -2,7 +2,9 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,27 +17,28 @@ public class JpaMain {
         tx.begin();
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
-            em.persist(member1);;
+            member1.setTeam(team);
+            em.persist(member1);
+            ;
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
             //language=HQL
-            String query = "select group_concat(m.username) from Member m";
+            String query = "select m.username from Team t join t.members m"; // FROM절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색가능
 
             List<String> resultList = em.createQuery(query, String.class).getResultList();
-
-            for (String s : resultList) {
-                System.out.println("s = " + s);
-
-            }
-
+            System.out.println("resultList = " + resultList);
             tx.commit();
 
         } catch (Exception e) {
